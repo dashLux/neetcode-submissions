9b8eae7e02154cbbd3560fcb16c83b6fbc27c699
Yiyang Zhou <yiyang.zhou95@gmail.com>
2026-08-04 15:02:29 -0700

    Add: non-cyclical-number - submission-2

---
+++ Data Structures & Algorithms/non-cyclical-number/submission-2.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean isHappy(int n) {
+        Set<Integer> seen = new HashSet<>();
+        int curr = n;
+        while (curr != 1) {
+            curr = sum(curr);
+            //System.out.println(curr);
+            if (seen.contains(curr)) {
+                return false;
+            }
+            seen.add(curr);
+        }
+        return true;
+        
+    }
+
+    public int sum(int n) {
+        //System.out.println(n);
+        int sum = 0;
+        while (n > 0) {
+            System.out.println(n);
+            int digit = n%10;
+            sum += digit * digit;
+            n /= 10;
+        }
+        return sum;
+    }
+}

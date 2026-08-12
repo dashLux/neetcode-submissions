8185ef06dde5ea49dbc62340fd87d93052acabee
Yiyang Zhou <yiyang.zhou95@gmail.com>
2026-08-11 23:34:31 -0700

    Add: meeting-schedule - submission-1

---
+++ Data Structures & Algorithms/meeting-schedule/submission-1.java
@@ -0,0 +1,25 @@
+/**
+ * Definition of Interval:
+ * public class Interval {
+ *     public int start, end;
+ *     public Interval(int start, int end) {
+ *         this.start = start;
+ *         this.end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+    public boolean canAttendMeetings(List<Interval> intervals) {
+        //sort all the intervals by start time
+        Collections.sort(intervals, (a, b) -> a.start - b.start);
+
+        for(int i = 0; i < intervals.size()-1; i++) {
+            if (intervals.get(i+1).start < intervals.get(i).end) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}

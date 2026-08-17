030ef009f3dda25db3cfb8d7905f296605d12693
Yiyang Zhou <yiyang.zhou95@gmail.com>
2026-08-16 22:45:08 -0700

    Add: lru-cache - submission-5

---
+++ Data Structures & Algorithms/lru-cache/submission-5.java
@@ -0,0 +1,81 @@
+class Node {
+    int key;
+    int value;
+    Node next;
+    Node prev;
+
+    public Node(int key, int value) {
+        this.key = key;
+        this.value = value;
+    }
+}
+class LRUCache {
+    Map<Integer, Node> cache;
+    int capacity;
+    Node MRU;
+    Node LRU;
+    
+
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        this.cache = new HashMap<>();
+        this.MRU = new Node(-1, -1);
+        this.LRU = new Node(-1, -1);
+        LRU.next = MRU;
+        MRU.prev = LRU;
+    }
+    
+    public int get(int key) {
+        if (!cache.containsKey(key)) {
+            return -1;
+        }
+
+        //mark node as most recenly used
+        Node node = cache.get(key);
+        removeNode(node);
+        insertAtMRU(node);
+
+        return node.value;
+    }
+    
+    public void put(int key, int value) {
+        //if key exists, no size change, only need to move node to MRU
+        if (cache.containsKey(key)) {
+            Node node = cache.get(key);
+            node.value = value;
+            removeNode(node);
+            insertAtMRU(node);
+
+            return;
+        }
+
+        //creating new mapping
+        Node node = new Node(key, value);
+        cache.put(key, node);
+        if (cache.size() > capacity) {
+            //purge LRU node
+            Node lruNode = LRU.next;
+            LRU.next = lruNode.next;
+            lruNode.next.prev = LRU;
+
+            //remove entry from cache
+            cache.remove(lruNode.key);
+        }
+        insertAtMRU(node);
+        return;
+    }
+
+    private void insertAtMRU(Node node) {
+        //insert node to most recently used
+        MRU.prev.next = node;
+        node.prev = MRU.prev;
+        node.next = MRU;
+        MRU.prev = node;
+    }
+
+    private void removeNode(Node node) {
+        //remove node from original position
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+    }
+}

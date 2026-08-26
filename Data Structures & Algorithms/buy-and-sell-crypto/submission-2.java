90018a4529405cc7f03864599e07775d1f433ddf
Yiyang Zhou <yiyang.zhou95@gmail.com>
2026-08-26 10:36:58 -0700

    Add: buy-and-sell-crypto - submission-2

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-2.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int maxProfit = 0;
+
+        int left = 0;
+        int right = 0;
+
+        while (right < prices.length) {
+            if (prices[right] < prices[left]) {
+                left = right;
+            } 
+            else {
+                int profit = prices[right] - prices[left];
+                maxProfit = Math.max(maxProfit, profit);
+            }
+            right++;
+        }
+
+        return maxProfit;
+    }
+}

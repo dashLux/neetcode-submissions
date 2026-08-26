cce107a4a8a63d2de41c326595b36ec4abbc2cc8
Yiyang Zhou <yiyang.zhou95@gmail.com>
2026-08-26 14:36:56 -0700

    Add: minimum-stack - submission-4

---
+++ Data Structures & Algorithms/minimum-stack/submission-4.java
@@ -0,0 +1,33 @@
+class MinStack {
+    Stack<Integer> stack;
+    Stack<Integer> minStack;
+
+    public MinStack() {
+        this.stack = new Stack<>();
+        this.minStack = new Stack<>();
+        
+    }
+    
+    public void push(int val) {
+        stack.push(val);
+        if (minStack.isEmpty()) {
+            minStack.push(val);
+        }
+        else {
+            minStack.push(Math.min(val, minStack.peek()));
+        }
+    }
+    
+    public void pop() {
+        minStack.pop();
+        stack.pop();
+    }
+    
+    public int top() {
+        return stack.peek();
+    }
+    
+    public int getMin() {
+        return minStack.peek();
+    }
+}

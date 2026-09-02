a58bb559b6a031455d9eb6d2fcfceb980a29de7c
Yiyang Zhou <yiyang.zhou95@gmail.com>
2026-09-02 15:49:45 -0700

    Add: search-for-word - submission-2

---
+++ Data Structures & Algorithms/search-for-word/submission-2.java
@@ -0,0 +1,40 @@
+class Solution {
+    public boolean exist(char[][] board, String word) {
+        int row = board.length;
+        int col = board[0].length;
+
+        for(int i = 0; i < row; i++) {
+            for(int j = 0; j < col; j++) {
+                if (board[i][j] == word.charAt(0)) {
+                    boolean found = dfs(board, i, j, 0, word);
+                    if (found) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    private boolean dfs(char[][] board, int r, int c, int index, String word) {
+        if (index == word.length()) {
+            return true;
+        }
+
+        if (r < 0 || r >= board.length || c < 0 || c >= board[0].length || board[r][c] != word.charAt(index)) {
+            return false;
+        }
+
+        char temp = board[r][c];
+        board[r][c] = '&';
+
+        boolean found =  dfs(board, r+1, c, index+1, word)
+                        ||dfs(board, r-1, c, index+1, word)
+                        ||dfs(board, r, c+1, index+1, word)
+                        ||dfs(board, r, c-1, index+1, word);
+        
+        board[r][c] = temp;
+        return found;
+
+    }
+}
